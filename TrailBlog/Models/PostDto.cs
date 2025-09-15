e6bdfca0e466807f7b4b76767042038943c0cb89
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-09-15 16:43:56 +0800

    Added required annotation in register and post DTO

--- TrailBlog/Models/PostDto.cs
+++ TrailBlog/Models/PostDto.cs
@@ -1,11 +1,22 @@
-﻿namespace TrailBlog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrailBlog.Models
 {
     public class PostDto
     {
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Content is required")]
         public string Content { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Author is required")]
         public string Author { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Community Name is required")]
         public string CommunityName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Community Id is required")]
         public Guid CommunityId { get; set; } = Guid.Empty;
     }
 }
--- TrailBlog/Models/RegisterDto.cs
+++ TrailBlog/Models/RegisterDto.cs
@@ -1,9 +1,16 @@
-﻿namespace TrailBlog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrailBlog.Models
 {
     public class RegisterDto
     {
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; } = string.Empty;
     }
 }
