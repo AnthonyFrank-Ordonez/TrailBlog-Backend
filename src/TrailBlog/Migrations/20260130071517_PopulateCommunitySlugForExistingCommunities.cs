863c3463541850e41e25cc3e3f1a9573339ec6c3
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-01-30 15:20:05 +0800

    fix: Added migrations to populate communities without slug

---
+++ src/TrailBlog/Migrations/20260130071517_PopulateCommunitySlugForExistingCommunities.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrailBlog.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateCommunitySlugForExistingCommunities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Communities""
+                SET ""CommunitySlug"" = LOWER(REPLACE(""Name"", ' ', '-'))
+                WHERE ""CommunitySlug"" IS NULL OR ""CommunitySlug"" = ''
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Communities""
+                SET ""CommunitySlug"" = NULL
+                WHERE ""CommunitySlug"" != ''
+            ");
+        }
+    }
+}
